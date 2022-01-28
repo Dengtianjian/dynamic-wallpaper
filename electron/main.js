8db8358654e3e15bbb643d2d44f1b28a194cced1
tianjian <mail@isdthj.com>
2022-01-28 21:10:07 +0800

    优化初始化时窗口大小

--- electron/main.js
+++ electron/main.js
@@ -9,7 +9,10 @@ function createMainWindow() {
   mainWindow = new BrowserWindow({
     width: Math.ceil(width * 0.8),
     height: Math.ceil(height * 0.8),
-    resizable: false,
+    // resizable: false,
+    maximizable: true,
+    minHeight: Math.ceil(height * 0.6),
+    minWidth: Math.ceil(width * 0.6),
     webPreferences: {
       preload: path.join(__dirname, "preload.js")
     }
