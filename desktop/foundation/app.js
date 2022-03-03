b6564004825d87a26e4c4a3dfdeca754c5895f53
tianjian <mail@isdtj.com>
2022-03-03 15:57:05 +0800

    主线程On取消传event参数

--- desktop/foundation/app.js
+++ desktop/foundation/app.js
@@ -39,7 +39,7 @@ module.exports.App = class {
   }
   on(channel, listener) {
     ipcMain.on(channel, (event, args) => {
-      listener.call(this, ...args, event);
+      listener.call(this, ...args);
     });
 
     return this;
