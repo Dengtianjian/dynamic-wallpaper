e5f6a82e84c9c11cbcd5a1bd69b7b814f807e977
tianjian <mail@isdtj.com>
2022-03-04 17:41:12 +0800

    优化query

--- src/foundation/attachment.ts
+++ src/foundation/attachment.ts
@@ -1,10 +1,21 @@
 import config from "../config";
+import http from "./http";
 
 export default {
   genDownloadUrl(fileId: string) {
     return `${config.api.url}/downloadAttachment?fileId=${escape(fileId)}`;
   },
-  genImageThumbUrl(fileId: string, width?: number, height?: number) {
-    return `${config.api.url}/thumbnail?fileId=${escape(fileId)}&width=${width}&height=${height}`;
+  genImageThumbUrl(fileId: string, width: number = -1, height: number = -1) {
+    const querys: Record<string, string> = {
+      fileId: escape(fileId)
+    };
+    if (width > 0) {
+      querys['width'] = width.toString();
+    }
+    if (height > 0) {
+      querys['height'] = height.toString();
+    }
+    const query: string = http.generateQuery(querys);
+    return `${config.api.url}/thumbnail?${query}`;
   }
 }
